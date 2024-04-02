5f8a61123fa48436fc10e43afac601761866dc79
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-02 13:09:58 +0530

    Update variables.tf

--- secrets/variables.tf
+++ secrets/variables.tf
@@ -41,11 +41,11 @@ variable "eeano" {
 }
 
 
-/*variable "secret_masterDB" {
+variable "dbUser" {
     description = "database administrator password"
     type = string
     sensitive = true
-}*/
+}
 
 variable "env" {}
 
