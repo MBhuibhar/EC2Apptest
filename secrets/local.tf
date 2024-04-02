fd5e98b5d2543feb33a78deaa3558a4b4eae5674
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-02 13:05:17 +0530

    Update local.tf

--- secrets/local.tf
+++ secrets/local.tf
@@ -1,3 +1,3 @@
 locals {
-  db_cred = jsondecode(str(data.aws_secretsmanager_secret_version.dbUser.secret_string))
+  db_cred = jsondecode(data.aws_secretsmanager_secret_version.dbUser.secret_string)
 }
