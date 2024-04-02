b2690b985151b3bddedc5170798c26e38d94ba10
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-02 12:53:19 +0530

    Update main.tf

--- secrets/main.tf
+++ secrets/main.tf
@@ -4,17 +4,17 @@
 
 }*/
 
-resource "aws_secretsmanager_secret" "secret_masterDB" {
+resource "aws_secretsmanager_secret" "dbUser" {
     name = "pite-dldeb-${var.db}-${var.env}-admin"
     
 }
 
-resource "aws_secretsmanager_secret_version" "creds" {
-    secret_id = aws_secretsmanager_secret.secret_masterDB.id
+resource "aws_secretsmanager_secret_version" "dbUser" {
+    secret_id = aws_secretsmanager_secret.dbUser.id
     secret_string = <<EOF
     {
         "username": "adminaccount"
-        "password": "var.secret_masterDB"
+        "password": "var.dbUser"
     }
  EOF   
 }
