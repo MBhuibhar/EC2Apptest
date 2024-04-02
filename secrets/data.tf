002e7b5fb43255baf2553accaad710efb284513a
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-02 11:37:19 +0530

    Update data.tf

--- secrets/data.tf
+++ secrets/data.tf
@@ -6,3 +6,15 @@ data "aws_secretsmanager_secret" "secret_masterDB" {
 data "aws_secretsmanager_secret_version" "creds" {
     secret_id = tolist(data.aws_secretsmanager_secret.secret_masterDB.arn)[0]
 }
+data "aws_iam_policy_document" "ecs_task_execution_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue"
+    ]
+    resources = [
+      tolist(data.aws_secretsmanager_secrets.gcs_kafka_connect_docdb_user.arns)[0],
+      data.aws_secretsmanager_secret.gcs_kafka_secrets.arn
+    ]
+  }
+}
