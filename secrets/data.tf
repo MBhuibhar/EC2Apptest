275925fb757ec9063e31bc0bc651ecba1fd13740
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-03 14:46:00 +0530

    Update data.tf

--- secrets/data.tf
+++ secrets/data.tf
@@ -28,6 +28,7 @@ data "aws_secretsmanager_secrets" "dbUser" {
 
 data "aws_secretsmanager_secret_version" "dbUser" {
   secret_id = data.aws_secretsmanager_secrets.dbUser.arn
+  secret_string = var.dbUser
 }
 
 /*data "aws_secretsmanager_secret" "gcs_kafka_secrets" {
