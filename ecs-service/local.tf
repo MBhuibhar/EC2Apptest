a13f12129fee3f1dda9ee5d541218ed8f4a12638
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-05 15:02:06 +0530

    Delete ecs-service directory

--- ecs-service/local.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  global_name = "pite-dldeb-${var.service_name}-${var.env}-debezium-kafka"
-  topic_environments = {
-    dev   = "deveu"
-    stage = "stageeu"
-    prod  = "prodeu"
-  }
-}
