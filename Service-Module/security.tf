5d9c541b6710db98237812bd2d05a68f35472fa1
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-10 14:11:17 +0530

    Update security.tf

--- Service-Module/security.tf
+++ Service-Module/security.tf
@@ -1,33 +1,3 @@
-resource "aws_iam_role" "task_execution_role" {
-  name               = "${var.service_name}-ecs-task-execution-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_assume_policy.json
-
-  inline_policy {
-    name   = "module-managed-execution-role-policy"
-    policy = data.aws_iam_policy_document.ecs_module_managed_execution_role_policy.json
-  }
-
-  dynamic "inline_policy" {
-    for_each = var.task_execution_role_policy == "" ? [] : [1]
-    content {
-      name   = "user-managed-execution-role-policy"
-      policy = var.task_execution_role_policy
-    }
-  }
-}
-
-resource "aws_iam_role" "task_role" {
-  name               = "${var.service_name}-ecs-task-role"
-  assume_role_policy = data.aws_iam_policy_document.ecs_assume_policy.json
-
-  dynamic "inline_policy" {
-    for_each = var.task_role_policy == "" ? [] : [1]
-    content {
-      name   = "task-role-policy"
-      policy = var.task_role_policy
-    }
-  }
-####################################
 resource "aws_iam_role" "iam_role_ecs_terraform" {
   name = "pite-dldeb-${var.env}-iam-role"
   
