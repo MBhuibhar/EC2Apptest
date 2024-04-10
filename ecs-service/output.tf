5f6021c4b9ce29b0b35340dd371c0fda68f81135
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-10 20:02:11 +0530

    Update output.tf

--- ecs-service/output.tf
+++ ecs-service/output.tf
@@ -17,6 +17,4 @@ output "MYSQL_service_ecr_repo_url" {
 output "MYSQL_service_ecr_repo_arn" {
     value     = module.pite-dldeb_services_MYSQL_service.ecr_repository_arn
 }
-output "ecs_role" {
-  value = module.service-Module.aws_iam_role.iam_role_ecs_terraform.id
-}
+
