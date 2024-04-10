14b5f63ed8cd13da8dadedfe2a50667b6401054e
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-10 16:53:01 +0530

    Update security.tf

--- Service-Module/security.tf
+++ Service-Module/security.tf
@@ -172,7 +172,7 @@ resource "aws_iam_policy" "pite-dldeb-pullfromecr" {
 # # AWS IAM role policy attachment
 # #################################################################
 
-resource "aws_iam_role_policy_attachment" "SecretsManagerReadWrite" {
+/*resource "aws_iam_role_policy_attachment" "SecretsManagerReadWrite" {
   role       = aws_iam_role.iam_role_ecs_terraform.name
   policy_arn = data.aws_iam_policy.SecretsManagerReadWrite.arn
 }
@@ -205,4 +205,4 @@ resource "aws_iam_role_policy_attachment" "pite-dldeb-infra-setup-policy_dlake" 
 resource "aws_iam_role_policy_attachment" "pite-dldeb-pullfromecr_dlake" {
   role       = aws_iam_role.iam_role_ecs_terraform.name
   policy_arn = aws_iam_policy.pite-dldeb-pullfromecr.arn
-}
+}*/
