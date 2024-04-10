f259e3d76f9f96d02ae8abc83858e47af05a6353
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-10 20:01:52 +0530

    Update main.tf

--- ecs-service/main.tf
+++ ecs-service/main.tf
@@ -10,8 +10,8 @@ module "pite-dldeb_services_SQL_service" {
   container_port                 = 8083
   cpu                            = var.fargate_resources.pite-dldeb_services_SQL_all_service.cpu
   memory                         = var.fargate_resources.pite-dldeb_services_SQL_all_service.memory
-  task_role_policy               = module.aws_iam_role.iam_role_ecs_terraform.ecs_role.arn
-  task_execution_role_policy     = module.aws_iam_role.iam_role_ecs_terraform.ecs_role.arn
+  task_role_policy               = data.aws_iam_role.iam_role_ecs_terraform.arn
+  task_execution_role_policy     = data.aws_iam_role.iam_role_ecs_terraform.arn
   container_traffic_protocol     = "tcp"
   account_id                     = ""
 
