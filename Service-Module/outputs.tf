aa343c034a319361b6fd52df55bcb60b832dbfbf
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-10 14:10:27 +0530

    Update outputs.tf

--- Service-Module/outputs.tf
+++ Service-Module/outputs.tf
@@ -14,6 +14,6 @@ output "ecr_repository_url" {
 }
 
 output "ecr_repository_arn" {
-  value       = var.ecr_image == "" ? "local.ecr_repository_name.arn" : ""
+  value       = var.ecr_image == "" ? local.ecr_repository_name.arn : ""
   description = "If managed, the ARN of the ECR repository"
 }
