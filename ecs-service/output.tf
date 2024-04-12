716d87baace1bd69e20db9bdba3a3c0c50f30b1f
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-12 13:18:08 +0530

    Update output.tf

--- ecs-service/output.tf
+++ ecs-service/output.tf
@@ -11,10 +11,14 @@ output "sql_service_ecr_repo_arn" {
 output "MYSQL_service_ecr_repo_url" {
   value       = module.pite-dldeb_services_MYSQL_service.ecr_repository_url
   description = "DB service ECR repository ARN"
-    
+
 }
 
 output "MYSQL_service_ecr_repo_arn" {
-    value     = module.pite-dldeb_services_MYSQL_service.ecr_repository_arn
+  value = module.pite-dldeb_services_MYSQL_service.ecr_repository_arn
+}
+
+output "ecs_role" {
+  value = module.service-Module.aws_iam_role.iam_role_ecs_terraform.id
 }
 
