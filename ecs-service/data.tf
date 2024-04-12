aa06e6dff041cf96207dbfb82f51853bdfc06065
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-12 13:22:21 +0530

    Update data.tf

--- ecs-service/data.tf
+++ ecs-service/data.tf
@@ -25,3 +25,30 @@ data "aws_iam_policy" "pite-dldeb-infra-setup-policy" {
 data "aws_iam_policy" "pite-dldeb-pullfromecr" {
   name = "pite-dldeb-${var.env}-pullfromecr"
 }
+data "aws_secretsmanager_secret" "masterDB" {
+    arn = aws_secretsmanager_secret.masterDB.arn
+  
+}
+
+data "aws_secretsmanager_secret_version" "creds" {
+    secret_id = data.aws_secretsmanager_secret.masterDB.id
+}
+
+data "aws_iam_policy_document" "ecs_task_execution_role_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "secretsmanager:GetSecretValue"
+    ]
+    resources = [
+      data.aws_secretsmanager_secret.masterDB.arn
+    ]
+  }
+}
+
+data "aws_secretsmanager_secrets" "masterDB" {
+  filter {
+    name   = "name"
+    values = ["pite-dldeb-${var.service_name}-${var.env}-admin"]
+  }
+}
