f0d0fe7a859c8b2bfe3ab3af3912d61545a8b654
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-12 12:55:47 +0530

    Update locals.tf

--- Service-Module/locals.tf
+++ Service-Module/locals.tf
@@ -1,5 +1,5 @@
 locals {
-  ecr_repository_name = data.aws_ecr_repository.ecr_repo.name
+  ecr_repository_name = data.aws_ecr_repository.ecr_repo.arn
   ecr_latest_image    = data.aws_ecr_repository.ecr_repo.name == "" ? "latest" : data.aws_ecr_repository.ecr_repo.name
   container_definition = {
     name        = var.service_name
@@ -26,4 +26,26 @@ locals {
   }
   private_subnet_ids = [for k, v in data.aws_subnets.private_subnets : length(v.ids) > 0 ? v.ids[0] : ""]
 }
-
+locals {
+  role_name = "pite-dldeb-${var.env}-ecs-iam-role"
+}
+/*locals {
+  env = "dev"
+  dev = {             
+  security_group  = data.aws_security_group.ecs_dev_sg.name                     #need to set condition for prod
+  vpc_id          = "vpc-0be9984772fa33a6b"                                      #need to check condition for prod
+  private_subnet1 = "subnet-09dacc0e93e6e65e8"
+  private_subnet2 = "subnet-0b8d8a23586b6560e"
+  aws-aws_region  = "eu-central-1"
+  }
+}
+locals {
+  env  = "prod"
+  prod = {
+  security_group  = data.aws_security_group.ecs_prod_sg.name                     
+  vpc_id          = ""                                      
+  private_subnet1 = ""
+  private_subnet2 = ""
+  aws-aws_region  = "eu-central-1"
+  }
+}*/
