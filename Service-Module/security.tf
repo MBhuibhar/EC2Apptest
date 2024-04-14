76561d7caef7ea8c2d8897c1b80d21edb1a028cd
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-14 12:10:15 +0530

    Update security.tf

--- Service-Module/security.tf
+++ Service-Module/security.tf
@@ -17,7 +17,7 @@
     ]
   }
   EOF
-}
+}*/
 
 # #################################################################
 # # AWS IAM role policy
@@ -39,7 +39,7 @@ data "aws_iam_policy" "AmazonSNSFullAccess" {
   arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
 }*/
 
-resource "aws_iam_policy" "pite-dldeb-Pull_image_dlake" {
+*/resource "aws_iam_policy" "pite-dldeb-Pull_image_dlake" {
   name = "pite-dldeb-${var.env}-Pull_image_dlake"
 
   policy = jsonencode({
@@ -183,13 +183,13 @@ resource "aws_iam_policy" "pite-dldeb-pullfromecr" {
       }
     ]
   })
-}
+}*/
 
 # #################################################################
 # # AWS IAM role policy attachment
 # #################################################################
 
-resource "aws_iam_role_policy_attachment" "SecretsManagerReadWrite" {
+/*resource "aws_iam_role_policy_attachment" "SecretsManagerReadWrite" {
   role       = aws_iam_role.iam_role_ecs_terraform.name
   policy_arn = data.aws_iam_policy.SecretsManagerReadWrite.arn
 }
