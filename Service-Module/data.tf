a9afcd65b3118ea0a5ffa8299bde84fc5df59acb
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-14 14:19:22 +0530

    Update data.tf

--- Service-Module/data.tf
+++ Service-Module/data.tf
@@ -90,9 +90,9 @@ data "aws_security_group" "ecs_sg" {
   vpc_id      = var.vpc_id
 }
 
-data "aws_iam_role" "iam_role_ecs_terraform" {
+/*data "aws_iam_role" "iam_role_ecs_terraform" {
   name = var.iam_role
-}
+}*/
 data "aws_iam_policy" "SecretsManagerReadWrite" {
   arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
 }
@@ -107,4 +107,9 @@ data "aws_iam_policy" "CloudWatchAgentServerPolicy" {
 
 data "aws_iam_policy" "AmazonSNSFullAccess" {
   arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
+}
+data "aws_ecr_repository" "ecr_repo" {
+  name = var.ecr_repo
+  repository_url = "aws_account_id.dkr.ecr.region.amazonaws.com/pite.dldeb-${var.env}-${var.db}-ecr-repo"
+  
 }
