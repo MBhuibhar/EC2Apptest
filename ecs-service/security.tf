998d5263ede26289999270154f32bf2b609f4588
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-15 14:27:30 +0530

    Update security.tf

--- ecs-service/security.tf
+++ ecs-service/security.tf
@@ -3,3 +3,18 @@ module "iam_role_policy" {
     account_id = ""
     env = ""
 }
+data "aws_iam_policy" "SecretsManagerReadWrite" {
+  arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+}
+
+data "aws_iam_policy" "AmazonSQSFullAccess" {
+  arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
+}
+
+data "aws_iam_policy" "CloudWatchAgentServerPolicy" {
+  arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
+}
+
+data "aws_iam_policy" "AmazonSNSFullAccess" {
+  arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
+}
