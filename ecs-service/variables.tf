9ac20bc2cac45344dd91a7bdbba34b8fed43a595
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-15 20:37:06 +0530

    Update variables.tf

--- ecs-service/variables.tf
+++ ecs-service/variables.tf
@@ -2,10 +2,10 @@
 ##### Control Variables ############
 ####################################
 
-variable "account_name" {
+/*variable "account_name" {
   type        = string
   description = "Name of the account we want to deploy to"
-}
+}*/
 
 variable "account_selector" {
   description = "Select the account we will be working on"
