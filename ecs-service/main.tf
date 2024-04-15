39d258a31bdca15e90139cdeaadc9efb8c4b218a
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-15 16:26:45 +0530

    Update main.tf

--- ecs-service/main.tf
+++ ecs-service/main.tf
@@ -9,7 +9,7 @@ resource "aws_cloudwatch_log_group" "ecs_logs" {
 module "pite-dldeb_services_SQL_service" {
   source                     = "../Service-Module/"
   service_name               = "${local.global_name}-sql-service"
-  ecs_cluster_name           = "${var.account_name}-${var.env}-cluster"
+  ecs_cluster_name           = "pite-dldeb-${var.env}-ecs-cluster"
   vpc_id                     = var.vpc_id
   container_port             = 8083
   cpu                        = var.fargate_resources.pite-dldeb_services_SQL_all_service.cpu
@@ -213,7 +213,7 @@ module "pite-dldeb_services_SQL_service" {
 module "pite-dldeb_services_MYSQL_service" {
   source                     = "../Service-Module/"
   service_name               = "${local.global_name}-dldeb-mysql-service"
-  ecs_cluster_name           = "${var.account_name}-${var.env}-cluster"
+  ecs_cluster_name           = "pite-dldeb-${var.env}-ecs-cluster"
   vpc_id                     = var.vpc_id
   container_port             = 8083
   cpu                        = var.fargate_resources.pite-dldeb_services_MYSQL_all_service.cpu
