6a74a6c2976b742415774a8db2e9fb1195200ee3
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-15 12:53:01 +0530

    Update security.tf

--- ecs-service/security.tf
+++ ecs-service/security.tf
@@ -1,3 +1,15 @@
 module "iam_assumable_role" {
-    source = "../Service-Module/"
+    source = "../Service-Module/security.tf"
+    output "iam_role_ecs" {
+        value = module.service-Module.iam_role
+    }
+    output "Pull_image" {
+        value = module.service-Module.Pull_image_dlake
+    }
+    output "infra-setup" {
+        value = module.serivice-Module.infra-setup-policy
+    }
+    output "pullecr" {
+        value = module.service-Module.pullfromecr
+    }
 }
