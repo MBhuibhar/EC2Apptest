7583a3214861e0c17948cb017acb7f053b3c412b
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-15 13:58:50 +0530

    Update security.tf

--- ecs-service/security.tf
+++ ecs-service/security.tf
@@ -1,15 +1,5 @@
 module "iam_role_policy" {
     source = "../Service-Module/iam_role/"
-    account_id = {
-        default = {
-            "dev" = "904626469165" 
-            "stage" = "904626469165"
-        }
-    }
-    env = {
-        default = {
-            "env" = ["deveu",stageeu,prodeu]
-
-        }
-    }
+    account_id = ""
+    env = ""
 }
