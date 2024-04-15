ff5aed1c12a1b4d7d6440da031a8caa1c7ebecf4
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-15 14:37:03 +0530

    Update data.tf

--- ecs-service/data.tf
+++ ecs-service/data.tf
@@ -1,18 +1,3 @@
-data "aws_iam_policy" "SecretsManagerReadWrite" {
-  arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
-}
-
-data "aws_iam_policy" "AmazonSQSFullAccess" {
-  arn = "arn:aws:iam::aws:policy/AmazonSQSFullAccess"
-}
-
-data "aws_iam_policy" "CloudWatchAgentServerPolicy" {
-  arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
-}
-
-data "aws_iam_policy" "AmazonSNSFullAccess" {
-  arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
-}
 data "aws_iam_role" "iam_role_ecs_terraform" {
   name = var.iam_role
 }
