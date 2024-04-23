81c0e918dd95c58d93044113761254c91df79818
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-23 17:01:34 +0530

    Update data.tf

--- Service-Module/data.tf
+++ Service-Module/data.tf
@@ -97,6 +97,6 @@ data "external" "ecr_latest_image" {
 /*data "aws_ecr_repository" "ecr_image" {
   name = var.ecr_image
 }*/
-data "aws_ecr_repository" "ecr_repo.arn" {
+data "aws_ecr_repository" "ecr_repo" {
   name = "${var.account_id}.dkr.ecr.eu-west-1.amazonaws.com/pite-dldeb-${var.env}-${var.service_name}-ecr-repo"
 }
