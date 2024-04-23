72b2e14df6d089b8c3a23333d10ece80d19ecd67
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-23 21:06:50 +0530

    Update local.tf

--- ecs-service/local.tf
+++ ecs-service/local.tf
@@ -1,5 +1,5 @@
 locals {
-  ecr_repository_name = "${var.account_id}.dkr.ecr.eu-west-1.amazonaws.com/pite-dldeb-${var.env}-${var.service_name}-ecr-repo
+  ecr_repository_name = "${var.account_id}.dkr.ecr.eu-west-1.amazonaws.com/pite-dldeb-${var.env}-${var.service_name}-ecr-repo"
   global_name = "pite-dldeb-${var.service_name}-${var.env}-debezium-kafka"
   topic_environments = {
     dev   = "deveu"
