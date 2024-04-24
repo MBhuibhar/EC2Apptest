980863897feb4bafdece90f8c3bc5e6d90e3771d
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-24 17:27:07 +0530

    Update output.tf

--- ecs-service/output.tf
+++ ecs-service/output.tf
@@ -7,6 +7,11 @@ output "sql_service_ecr_repo_arn" {
   value       = module.pite-dldeb-SQL.ecr_repository_arn     ##pite-dldeb_services_SQL_service
   description = "DB service ECR repository ARN"
 }
+
+output "secret_value" {
+  value = data.aws_secretsmanager_secret_version.creds.secret_string
+  sensitive = true
+}
  /*output "ecr_repository_url" {
    value      = data.aws_ecr_repository.ecr_repo.repository_url
 }*/
