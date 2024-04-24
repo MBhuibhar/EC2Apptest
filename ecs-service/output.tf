4548385fe5fd9be2223a56254dd24e2f2b363f59
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-24 11:15:06 +0530

    Update output.tf

--- ecs-service/output.tf
+++ ecs-service/output.tf
@@ -7,9 +7,9 @@ output "sql_service_ecr_repo_arn" {
   value       = module.pite-dldeb_services_SQL_service.ecr_repository_arn
   description = "DB service ECR repository ARN"
 }
- output "ecr_repository_url" {
+ /*output "ecr_repository_url" {
    value      = data.aws_ecr_repository.ecr_repo.repository_url
-}
+}*/
 /*output "MYSQL_service_ecr_repo_url" {
   value       = module.pite-dldeb_services_MYSQL_service.ecr_repository_url
   description = "DB service ECR repository ARN"
