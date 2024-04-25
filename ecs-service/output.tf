b12ab1194086ab3dc425fed075798f4d8afa5f86
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-25 12:00:21 +0530

    Update output.tf

--- ecs-service/output.tf
+++ ecs-service/output.tf
@@ -13,9 +13,9 @@ output "secret_value" {
   sensitive = true
 }
 
-output "secret_name_valid" {
+/*output "secret_name_valid" {
   value = module.pite-dldeb-SQL      #data.aws_secretsmanager_secret_version.creds.secret_string
-}
+}*/
  /*output "ecr_repository_url" {
    value      = data.aws_ecr_repository.ecr_repo.repository_url
 }*/
