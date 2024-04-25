f29e39996a6523eb6264b7319fc6f8b430de6351
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-04-25 15:26:43 +0530

    Update local.tf

--- ecs-service/local.tf
+++ ecs-service/local.tf
@@ -8,5 +8,5 @@ locals {
   }
 }
 locals {
-  validate_secrets_name = data.aws_secretsmanager_secret_version.creds == local.global_name ? 1 : 0 #"global_name_secretnotmatch" 
+  validate_secrets_name = data.aws_secretsmanager_secret_version.creds == local.global_name ? 1 : 0 #"Please create secret using secret pipeline" 
 }
