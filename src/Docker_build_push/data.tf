35539bed5a21d92970b2fdc68010820d95a3bb95
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-05-02 20:46:46 +0530

    Update data.tf

--- src/Docker_build_push/data.tf
+++ src/Docker_build_push/data.tf
@@ -1,3 +1,10 @@
 data "aws_ecr_repository" "debezium_repository" {
   name = "pite-dldeb-${var.service_name}-${var.env}-debezium-kafka-service-ecr-repo"
 }
+
+data "docker_image" "debezium" {
+     name = "pite-dldeb-${var.service_name}-${var.env}-docker-image"
+}
+data "docker_container" "debezium" {
+     name  = "pite-dldeb-${var.service_name}-${var.env}-container"
+}
