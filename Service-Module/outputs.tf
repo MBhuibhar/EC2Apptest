2d1268c97fa93528b40208831cdd26c79e1077a2
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-05-02 13:58:39 +0530

    Update outputs.tf

--- Service-Module/outputs.tf
+++ Service-Module/outputs.tf
@@ -9,11 +9,11 @@ output "cloudwatch_log_group" {
 }
 
 output "ecr_repository_url" {
-  value       = local.aws_ecr_repository == "" ? "${local.ecr_repository_name}:latest" : local.aws_ecr_repository #var.ecr_image #local.ecr_repository_name.repository_url
+  value       = var.ecr_image == "" ? "${aws_ecr_repository.ecr_repo.repository_url}:latest" : var.ecr_image #local.ecr_repository_name.repository_url
   description = "ECR repository URL, either the provided one or the managed one"
 }
 
 output "ecr_repository_arn" {
-  value       = local.aws_ecr_repository == "" ? local.ecr_repository_name : ""   #var.ecr_image #ecr_repository_name.arn
+  value       = var.ecr_image == "" ? aws_ecr_repository.ecr_repo.arn : ""   #var.ecr_image #ecr_repository_name.arn
   description = "If managed, the ARN of the ECR repository"
 }
