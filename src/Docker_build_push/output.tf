3fc1e35530f1a29162b6af73911541fedeb2d566
MBhuibhar <153593521+MBhuibhar@users.noreply.github.com>
2024-05-02 20:48:37 +0530

    Update output.tf

--- src/Docker_build_push/output.tf
+++ src/Docker_build_push/output.tf
@@ -1,14 +1,14 @@
 output "dockerimage" {
-   value = docker_image.debezium
+   value = data.docker_image.debezium
 }
 
 output "dockercontainer" {
-  value = docker_container.debezium
+  value = data.docker_container.debezium
 }
 
-output "dockerbuild" {
+/*output "dockerbuild" {
   value = null_resource.docker_packaging
-}
+}*/
 output "debezium_repository" {
   value = data.aws_ecr_repository.debezium_repository.repository_url
 }
